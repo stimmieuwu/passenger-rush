c133257155e6a25176ce36e36549163635d9ceb0
simonee <semariquit@gmail.com>
2024-11-28 22:45:46 +0800

    added javadoc

--- MiniProj/src/application/Main.java
+++ MiniProj/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import map.GridMap;
@@ -12,16 +12,24 @@ import javafx.scene.layout.BorderPane;
 import mechanics.GameTimer; //temp
 import entities.Player;
 
-/***********************************************************
- * This program implements a game called "Passenger Rush" 
+/**
+ * This is the main class for the JavaFX application that implements a
+ * game called Passenger Rush.
  * 
  * @author Simonee Ezekiel M. Mariquit
  * @author Jan Zuriel Camba
  * @author Norman Marfa III
  * @created_date 2024-11-25 12:13 AM
- *
- ***********************************************************/
+ */
 public class Main extends Application {
+
+	/**
+	 * Override the start method of the Application class to create the main
+	 * application window, set the scene, and add a stylesheet.
+	 * 
+	 * @param primaryStage The primary stage for this application where scenes can
+	 *                     be set.
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -31,29 +39,34 @@ public class Main extends Application {
 			primaryStage.setTitle("Passenger Rush Test");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 //			gameTimer.setScene(scene);
-			
+
 			// to test tiles
 			// remember to remove import
-			
+
 			Canvas canvas = new Canvas(800, 800);
 			GraphicsContext gc = canvas.getGraphicsContext2D();
-	        root.getChildren().add(canvas);
-	        
-	        //Testing purposes of GameTImer
+			root.getChildren().add(canvas);
+
+			// Testing purposes of GameTImer
 			GameTimer gameTimer = new GameTimer(gc, scene);
 			gameTimer.start();
 //			GridMap map = new GridMap(gc);
-
+//
 //			map.drawMap(gc);
 
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	/**
+	 * Entry point to the JavaFX application
+	 * 
+	 * @param args Command line arguments, which for this application is not
+	 *             necessary
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
--- MiniProj/src/effects/Debuff.java
+++ MiniProj/src/effects/Debuff.java
@@ -1,7 +1,13 @@
 package effects;
 
 /**
- * Javadoc
+ * Parent class for debuffs in Passenger Rush.
+ * Whenever a jeepney picks a debuff up, it will inflict the debuff on the opponent.
+ * 
+ * @author Simonee Ezekiel M. Mariquit
+ * @author Jan Zuriel Camba
+ * @author Norman Marfa III
+ * @created_date 2024-11-25 12:13 AM
  */
 public class Debuff {
 
