fde8940401784b9a17149e9e99d47661fdee4ae3
BuildTools <unconfigured@null.spigotmc.org>
2024-12-07 22:24:26 +0800

    Documented Time Elapsed

--- MiniProj/src/mechanics/TimeElapsed.java
+++ MiniProj/src/mechanics/TimeElapsed.java
@@ -1,22 +1,33 @@
 package mechanics;
 
-import javafx.animation.AnimationTimer;
-
+/**
+ * Class for tracking how much time has elapsed in seconds by tracking when the
+ * game started, tracking the current time, and subtracting.
+ *
+ * @author Simonee Ezekiel M. Mariquit
+ * @author Jan Zuriel Camba
+ * @author Norman Marfa III
+ * @created_date 2024-12-09
+ */
 public class TimeElapsed {
 
-    private static long startTime;
-    private static int elapsedSeconds;
+	private static long startTime;
+	private static int elapsedSeconds;
+	private static final int NANO = 1_000_000_000;
+	
+	/** Start the timer */
+	public static void start() {
+		startTime = System.nanoTime();
+	}
 
-    public static void start() {
-        startTime = System.nanoTime();
-    }
+	/** Update the elapsed time by calling this in the AnimationTimer. */
+	public static void update() {
+		long now = System.nanoTime();
+		elapsedSeconds = (int) ((now - startTime) / NANO);
+	}
 
-    public static void update() {
-        long now = System.nanoTime();
-        elapsedSeconds = (int) ((now - startTime) / 1_000_000_000.0);
-    }
-
-    public static int getElapsedSeconds() {
-        return elapsedSeconds;
-    }
+	/** Get the elapsed time in seconds */
+	public static int getElapsedSeconds() {
+		return elapsedSeconds;
+	}
 }
